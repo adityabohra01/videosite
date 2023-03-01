c66d3a4b0c1b02ee8b122cbd67ea8239bee1be90
Aman KUmar <amankumar.spj410@gmail.com>
2023-03-02 02:54:35 +0530

    fixed private key

--- server/firebase/index.js
+++ server/firebase/index.js
@@ -2,7 +2,7 @@ import {initializeApp, cert} from "firebase-admin/app"
 import { config } from "dotenv"
 
 config()
-const credential = cert({ ...process.env })
+const credential = cert({ ...process.env, private_key: process.env.private_key.replace(/\\n/gm, "\n") })
 
 const firebaseApp = initializeApp({
     credential,
